dd052241a9740f9c126a8145aaf3b40fcd597db1
Lukas Andersen <luke@slate71.com>
2016-10-23 15:12:49 -0700

    Rewrite scrollspy and clean up waves

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,39 +1,36 @@
-window.addEventListener('scroll', function() {
-  var scrollTop = document.body.scrollTop;
-  var nav1 = document.querySelector('#nav1');
-  var nav2 = document.querySelector('#nav2');
-  var nav3 = document.querySelector('#nav3');
-  var sec1 = document.querySelector('#section1');
-  var sec2 = document.querySelector('#section2');
-  var sec3 = document.querySelector('#section3');
+(function() {
+  function scrollSpy() {
+    var navHeight = document.querySelector('nav').getBoundingClientRect().height;
 
-  function top(sec) {
-    return sec.getBoundingClientRect().top;
+    // Get all scroll sections
+    var scrollSections = [].slice.call(
+      document.querySelectorAll('.scroll-section')
+    );
+
+    var currentSectionIndex = scrollSections
+      .map(sec => sec.getBoundingClientRect().bottom)
+      .findIndex(bottom => bottom >= navHeight);
+
+    var currentSectionBackground = scrollSections
+      .map(sec => getBackground(sec))
+      [currentSectionIndex];
+
+    var scrollNavs = document.querySelectorAll('.scroll-nav')
+
+    scrollSections.forEach((sec, idx) => {
+      scrollNavs[idx].style.background = defaultBackground();
+    });
+
+    scrollNavs[currentSectionIndex].style.background = currentSectionBackground;
+
+    function getBackground(sec) {
+      return window.getComputedStyle(sec, null).getPropertyValue('background');
+    }
+
+    function defaultBackground() {
+      return "#e3919f";
+    }
   }
 
-  function background(sec) {
-    return window.getComputedStyle(sec, null).getPropertyValue('background');
-  }
-
-  function defaultBackground() {
-    return "#e3919f";
-  }
-
-  if (top(sec2) > 51.1875) {
-    nav1.style.background = background(sec1);
-    nav2.style.background = defaultBackground();
-    nav3.style.background = defaultBackground();
-  }
-
-  if (top(sec2) <= 51.1875 && top(sec3) > 51.1875) {
-    nav1.style.background = defaultBackground();
-    nav2.style.background = background(sec2);
-    nav3.style.background = defaultBackground();
-  }
-
-  if (top(sec3) <= 51.1875) {
-    nav1.style.background = defaultBackground();
-    nav2.style.background = defaultBackground();
-    nav3.style.background = background(sec3);
-  }
-});
+  window.addEventListener('scroll', scrollSpy);
+})();
